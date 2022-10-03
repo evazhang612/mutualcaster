2429811c3dc3831f03f6bbb3f5a0c5f93a16f1d4
Eva Zhang <evazhang@Evas-MacBook-Pro.local>
2022-10-03 16:11:29 -0400

    Adding html styling and some other routers, lol i should document this more

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from flask import Flask, jsonify,request, Response, render_template
+from flask import Flask, jsonify,request, Response, render_template, redirect, url_for
 from flask_restful import Resource
 from flask_cors import CORS 
 import requests
 import pandas as pd
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired
 import json
 from flask import render_template_string
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'vfapafwo68'
+Bootstrap(app)
+
+OPTIONS = ['mutuals','intros','commonfollowing', 'aremutuals', 'isfollowing']
+
+class MutualForm(FlaskForm):
+    name1 = StringField('User 1?', validators=[DataRequired()])
+    name2 = StringField('User 2?', validators=[DataRequired()])
+    option = SelectField(label='Option', 
+    			choices = OPTIONS, 
+    			validators = [DataRequired()]) 
+    submit = SubmitField('Submit')
 
 # terrible style done in like 15 mins please excuse this
 # todo: comments and like everything else beyond
@@ -23,18 +41,45 @@ def get_following(farcaster_address):
 	response=response.json()
 	return pd.json_normalize(response)
 
-@app.route('/',methods=("POST", "GET"))
+@app.route('/',methods=("GET","POST"))
 def hello():
-	try:
-		return {'data': "Up!"}
-	except:
-		return {'data': 'An Error Occurred during fetching Api'}
+	form = MutualForm()
+	if form.validate_on_submit():
+		return redirect( url_for(form.option.data, user1=form.name1.data, user2=form.name2.data) )
+	return render_template('index.html', form =form)
 
 def username_to_addy(name):
 	return requests.get("https://searchcaster.xyz/api/profiles?username="+name)
 
 def path_to_image_html(path):
-    return '<img src="'+ path + '" width="60" >'
+    return '<img src="'+ path + '" width="75" >'
+
+
+@app.route('/isfollowing/<user1>/<user2>')
+def isfollowing(user1, user2):
+	user1name = username_to_addy(user1).json()[0]['body']['address']
+	user2name = username_to_addy(user2).json()[0]['body']['address']
+	df1 = get_following(user1name)
+	isfollowing = user2 in list(df1.username)
+	if isfollowing: 
+		result = "yay!"
+	else:
+		result = "nay!"
+	return render_template('yayornay.html', result=result)
+
+@app.route('/aremutuals/<user1>/<user2>')
+def aremutuals(user1, user2):
+	user1name = username_to_addy(user1).json()[0]['body']['address']
+	user2name = username_to_addy(user2).json()[0]['body']['address']
+	df1 = get_following(user1name)
+	df2 = get_following(user2name)
+	is1following = user2 in list(df1.username)
+	is2following = user1 in list(df2.username)
+	if is1following and is2following: 
+		result = "yay!"
+	else:
+		result = "nay!"
+	return render_template('yayornay.html', result=result)
 
 @app.route('/mutuals/<user1>/<user2>')
 def mutuals(user1, user2):
@@ -52,7 +97,10 @@ def mutuals(user1, user2):
 	resultsdf = resultsdf[['username', 'displayName','image']]
 	print(resultsdf.to_html())
 	return render_template('result.html',  
-				tables=resultsdf.to_html(render_links=True,escape=False,index=False))
+				tables=resultsdf.to_html(justify= 'justify-all', col_space='75px',
+					render_links=True,escape=False,index=False,
+					classes=["table-bordered"]),
+				nmutuals = resultsdf.shape[0]) 
 
 @app.route('/intros/<user1>/<user2>')
 def intros(user1,user2):
@@ -66,7 +114,11 @@ def intros(user1,user2):
 	resultsdf['image'] = [path_to_image_html(i) for i in resultsdf['avatar.url']] 
 	resultsdf = resultsdf[['username', 'displayName','image']]
 	return render_template('result.html',  
-				tables=resultsdf.to_html(render_links=True,escape=False,index=False)) 
+				tables=resultsdf.to_html(
+					justify= 'justify-all', col_space='75px',
+					render_links=True,escape=False,index=False,
+					classes=["table-bordered"]),
+				nmutuals = resultsdf.shape[0]) 
 
 @app.route('/commonfollowing/<user1>/<user2>')
 def get(user1,user2):
@@ -80,7 +132,11 @@ def get(user1,user2):
 	resultsdf['image'] = [path_to_image_html(i) for i in resultsdf['avatar.url']] 
 	resultsdf = resultsdf[['username', 'displayName','image']]
 	return render_template('result.html',  
-				tables=resultsdf.to_html(render_links=True,escape=False,index=False))
+				tables=resultsdf.to_html(
+					justify= 'justify-all', col_space='75px',
+					render_links=True,escape=False,index=False,
+					classes=["table-bordered"]),
+				nmutuals = resultsdf.shape[0])
 
 if __name__ == '__main__':
 	app.run(debug=True, use_debugger=False, use_reloader=False,ssl_context='adhoc')
