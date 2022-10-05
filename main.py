6cd84340516544afd1a4e415f02c11a529ad0b9d
Eva Zhang <evazhang@Evas-MBP.lan>
2022-10-05 01:38:08 -0400

    Lowercase and people to follow

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vfapafwo68'
 Bootstrap(app)
 
-OPTIONS = ['mutuals','intros','commonfollowing', 'aremutuals', 'isfollowing']
+OPTIONS = ['mutuals','intros','commonfollowing',\
+			 'aremutuals', 'isfollowing', 'peopletofollow',
+			 'stats']
 
 class MutualForm(FlaskForm):
     name1 = StringField('User 1?', validators=[DataRequired()])
@@ -53,11 +55,11 @@ def hello():
 	return render_template('index.html', form =form)
 
 def username_to_addy(name):
+	name = name.lower()
 	return requests.get("https://searchcaster.xyz/api/profiles?username="+name)
 
 def path_to_image_html(path):
     return '<img src="'+ path + '" width="75" >'
-
 
 @app.route('/isfollowing/<user1>/<user2>')
 def isfollowing(user1, user2):
@@ -96,6 +98,24 @@ def mutuals(user1, user2):
 	print(df1)
 	print(df2)
 	moots = list(set(df1.username).intersection(set(df2.username)))
+	resultsdf = df1[df1['username'].isin(moots)]
+	resultsdf['image'] = [path_to_image_html(i) for i in resultsdf['avatar.url']] 
+	resultsdf = resultsdf[['username', 'displayName','image']]
+	print(resultsdf.to_html())
+	return render_template('result.html',  
+				tables=resultsdf.to_html(justify= 'justify-all', col_space='75px',
+					render_links=True,escape=False,index=False,
+					classes=["table-bordered"]),
+				nmutuals = resultsdf.shape[0]) 
+
+@app.route('/peopletofollow/<user1>/<user2>')
+def peopletofollow(user1, user2):
+		# return followers of user1 followed by user2
+	user1name = username_to_addy(user1).json()[0]['body']['address']
+	user2name = username_to_addy(user2).json()[0]['body']['address']
+	df1 = get_followers(user1name)
+	df2 = get_following(user2name)
+	moots = list(set(df1.username) - set(df2.username))
 	resultsdf = df1[df1['username'].isin(moots)]
 	resultsdf['image'] = [path_to_image_html(i) for i in resultsdf['avatar.url']] 
 	resultsdf = resultsdf[['username', 'displayName','image']]
